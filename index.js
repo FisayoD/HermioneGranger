33938674ea75fbfaaf736f4043096233b7ea9347
FisayoD <ojofisayo24@gmail.com>
2023-04-03 10:18:15 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const telegramBot = require("node-telegram-bot-api");
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const hermioneToken = process.env.HERMIONE;
+
+const OPENAI_API = process.env.OPENAI_API_KEY;
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API,
+  });
+const openai = new OpenAIApi(configuration);
+  
+
+const bot = new telegramBot(hermioneToken, {
+    polling: true
+})
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Hey, I am Hermione, best friend to Harry and Ron. What would you like to ask me today?');
+})
+
+bot.on("message", async (msg) => { 
+    const chatId = msg.chat.id;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: msg.text,
+        temperature: 0.9,
+        max_tokens: 1000,
+      });
+      const reply = response.data.choices[0].text;
+      bot.sendMessage(chatId, reply);
+})
+
+
